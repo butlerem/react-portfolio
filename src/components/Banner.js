6c5869f81c9d0d335d8eabbad78b73251572f8f3
butlerem <butlerem01@gmail.com>
2023-09-22 14:20:24 -0400

    Editing Banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -56,8 +56,7 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi! I'm Emma`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ ", Full-Stack Web Developer"]'><span className="wrap">{text}</span></span></h1>
-                  <p> </p>
-                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+                  <p> Hello and welcome! My name is Emma, and I'm an aspiring web developer located in Ottawa. Working towards completing the Web Development Bootcamp, I've cultivated a strong skill set that includes front-end and back-end technologies. I enjoy working with HTML, CSS, and JavaScript to build visually engaging user experiences. On the server-side, I have worked with Node.js and Express to develop scalable applications. Please have a look through my portfolio. </p>
               </div>}
             </TrackVisibility>
           </Col>
