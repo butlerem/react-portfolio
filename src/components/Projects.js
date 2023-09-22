5e4f5aba9ac831e02b63314742c0e18e95870693
butlerem <butlerem01@gmail.com>
2023-09-22 14:12:09 -0400

    Update projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,8 @@ import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
+import projImg4 from "../assets/img/project-img4.png";
+import projImg5 from "../assets/img/project-img5.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
@@ -11,35 +13,36 @@ export const Projects = () => {
   const projects = [
     {
       title: "PWA-Text-Editor ",
-      description: "Design & Development",
+      description: "",
       imgUrl: projImg1,
       url: "https://github.com/butlerem/PWA-Text-Editor"
     },
     {
       title: "E-Commerce Back-End",
-      description: "Design & Development",
+      description: "",
       imgUrl: projImg2,
+      url: "https://github.com/butlerem/E-commerce-Back-End"
     },
     {
       title: "Employee Tracker",
-      description: "Design & Development",
+      description: "",
       imgUrl: projImg3,
+      url: "https://github.com/butlerem/employee-tracker"
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
+      title: "Note Taker",
+      description: "",
+      imgUrl: projImg4,
+      url: "https://github.com/butlerem/note-taker"
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
+      title: "Work Day Scheduler",
+      description: "",
+      imgUrl: projImg5,
+      url: "https://github.com/butlerem/work-day-scheduler"
     },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
-    },
+  
+
   ];
 
   return (
@@ -51,7 +54,7 @@ export const Projects = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                 <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <p></p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
